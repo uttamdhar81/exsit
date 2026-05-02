e87936b28355d56cdc5b3f06fab8f82068ae6948
uttamdhar81 <uttamdhar81@gmail.com>
2026-05-02 15:01:13 +0530

    v65

--- inc/helpers.php
+++ inc/helpers.php
@@ -98,7 +98,7 @@ if ( ! function_exists( 'exsit_global_header' ) ) {
                                     echo exsit_theme_logo();
                                 echo '</div>';
 
-                                echo '<div class="menu-close" role="button" aria-label="<?php esc_attr_e('Close menu', 'exsit'); ?>" >';
+                                echo '<div class="menu-close" role="button" aria-label="' . esc_attr__( 'Close menu', 'exsit' ) . '">';
                                     echo '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>';                                
                                 echo '</div>';
                             echo '</div>';
